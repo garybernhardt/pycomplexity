1071471cf541faaed3fc445aa1cecef8d15d3168
Gary Bernhardt <gary.bernhardt@gmail.com>
2009-11-01 14:30:41 -0800

    Added missing "runtests.py" script

---
+++ runtests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import sys
+
+import nose
+
+
+if __name__ == '__main__':
+    nose_args = sys.argv + [r'-m',
+                            r'((?:^|[b_.-])(:?[Tt]est|When|describe|should|it))',
+                            r'--with-doctest',
+                            r'--doctest-extension=']
+    nose.run(argv=nose_args)
+
