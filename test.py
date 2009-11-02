29d39414343069d77c54173d71187525b290e406
Gary Bernhardt <gary.bernhardt@gmail.com>
2009-11-01 20:30:57 -0800

    Added a big, hairy integration test

--- test.py
+++ test.py
@@ -400,6 +400,30 @@ class describe_integration:
                 # implicit else
             """) == 4
 
+    def test_a_big_hairy_mess(self):
+        assert complexity(
+            """
+            while True: #1
+                if x and y or (z and w): #4
+                    try:
+                        x or y
+                        raise x
+                    except: #1
+                        5
+                    finally:
+                        break
+                else:
+                    [x for x in [x and y for x in y if z or w]] #5
+                try:
+                    return
+                except A: #1
+                    return
+                except B: #1
+                    (y for y in z) #1
+                finally:
+                    raise (x for x in z) #1
+            """) == 15
+
 
 def complexity(code):
     return Complexity(dedent(code)).score
